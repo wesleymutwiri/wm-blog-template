f2476fa5d4f360ddf9b4bad9d9814c4aff10792f
wesleymutwiri <wesleymochiemo@gmail.com>
2022-02-07 16:27:07 +0300

    added masonry layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,7 @@
 	</div>
 	<div class="column other-blogs">
 		<div class="blog-card">
-			<img src="https://source.unsplash.com/random/2" alt="" />
+			<img src="https://source.unsplash.com/random/300x200" alt="" />
 			<h4>Investing In CryptoCurrencies Quickstart</h4>
 			<p>Some weird descriptions here that shows small small description</p>
 		</div>
@@ -17,12 +17,17 @@
 			<p>Some weird descriptions here that shows small small description</p>
 		</div>
 		<div class="blog-card">
-			<img src="https://source.unsplash.com/random/4" alt="" />
+			<img src="https://source.unsplash.com/random/300x200?sig=3" alt="" />
 			<h4>Investing In CryptoCurrencies Quickstart</h4>
 			<p>Some weird descriptions here that shows small small description</p>
 		</div>
 		<div class="blog-card">
 			<img src="https://source.unsplash.com/random/5" alt="" />
+			<h4>Investing In CryptoCurrencies Quickstart</h4>
+			<p>Some weird descriptions here that shows small small description</p>
+		</div>
+		<div class="blog-card">
+			<img src="https://source.unsplash.com/random/6" alt="" />
 			<h4>Investing In CryptoCurrencies Quickstart</h4>
 			<p>Some weird descriptions here that shows small small description</p>
 		</div>
@@ -35,13 +40,28 @@
 		/* height: 100vh; */
 		overflow-y: hidden;
 	}
+	.recent-blog-image {
+		margin-right: 1rem;
+	}
 	.recent-blog-image img {
 		position: fixed;
 		top: 0;
 		z-index: -1;
-		width: 50%;
+		width: 49%;
 		height: 100%;
 		bottom: auto;
+	}
+
+	.recent-blog-image::after {
+		content: '';
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		width: 50%;
+		z-index: -1;
+		background: linear-gradient(to bottom, transparent 0%, var(--color-background) 100%);
 	}
 	.recent-blog-image h1 {
 		width: 500px;
@@ -53,24 +73,38 @@
 
 	.other-blogs {
 		width: 50%;
-		display: grid;
-		grid-template-columns: repeat(2, 1fr); 
+		/* display: grid; */
+		/* grid-template-columns: repeat(2, 1fr); 
 		grid-template-rows: repeat(auto-fit, minmax(30%, 1fr));
 		grid-template-rows: masonry;
-		grid-template-columns: masonry;
+		grid-template-columns: masonry; */
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 1rem 0.5rem;
+		/* grid-auto-flow: dense; */
+
+		/* gap: 10px; */
 	}
 
 	.blog-card {
-		width: 350px;
+		width: 100%;
 		border-radius: 3rem;
-		margin: 1rem;
 		height: max-content;
 	}
 	.blog-card h4 {
 		font-size: 1rem;
 	}
 	.blog-card img {
-		width: 100%;
+		width: 90%;
 		height: min-content;
+		object-fit: cover;
+	}
+
+	@supports (grid-template-rows: masonry) {
+		.other-blogs {
+			width: 50%;
+			display: grid;
+			grid-template-rows: masonry;
+		}
 	}
 </style>
